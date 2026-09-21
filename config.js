f21dc48a0f89ada23babe7d2cac6bf335fefc39f
JoshY95 <joshuayounan95@gmail.com>
2026-09-21 18:57:06 +1000

    Configure RingVault Sydney public client

---
+++ config.js
@@ -0,0 +1,4 @@
+window.RINGVAULT_CONFIG = Object.freeze({
+  supabaseUrl: "https://jvsvuxgpaqwlcstfuzbd.supabase.co",
+  supabasePublishableKey: "sb_publishable_mMBJlZuQJ0V_GcFSGBd38A_EoKVEPxQ",
+});
