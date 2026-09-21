4ae5c15cdb24762ceadb4a2202415e3c39b40918
JoshY95 <joshuayounan95@gmail.com>
2026-09-21 14:39:15 +1000

    Add reference image support to sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE = "ringvault-v1";
-const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.json", "data/catalogue.json"];
+const CACHE = "ringvault-v2";
+const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.json", "data/catalogue.json", "data/images.json"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+  if (new URL(event.request.url).origin !== self.location.origin) return;
   event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
     const copy = response.clone();
     caches.open(CACHE).then((cache) => cache.put(event.request, copy));
