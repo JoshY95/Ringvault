4f3a134494c475133874f4a952e77aa87e6c0058
JoshY95 <joshuayounan95@gmail.com>
2026-09-21 18:56:25 +1000

    Add secure account collection sync

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "ringvault-v2";
-const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.json", "data/catalogue.json", "data/images.json"];
+const CACHE = "ringvault-v3";
+const ASSETS = ["./", "index.html", "styles.css", "config.js", "app.js", "manifest.json", "data/catalogue.json", "data/images.json"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
